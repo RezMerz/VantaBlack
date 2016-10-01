21798695199644a8d6bb766cc25895f57c7a5a8b
Noise <rmirzaee95@gmail.com>
2016-10-01 18:53:09 +0330

    move completed

--- Assets/scripts/Interface.cs
+++ Assets/scripts/Interface.cs
@@ -21,20 +21,32 @@ public class Interface : MonoBehaviour {
             {
                 if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
-                    print("right");
+                    if(isEmpty(new Vector2(1, 0)))
+                    {
+                        engine.move(Direction.Right);
+                    }
                     
                 }
                 else if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
-                    engine.move(Direction.Left);
+                    if(isEmpty(new Vector2(-1, 0)))
+                    {
+                        engine.move(Direction.Left);
+                    }
                 }
                 else if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
-                    engine.move(Direction.Down);
+                    if(isEmpty(new Vector2(0, -1)))
+                    {
+                        engine.move(Direction.Down);
+                    }
                 }
                 else if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
-                    engine.move(Direction.Up);
+                    if(isEmpty(new Vector2(0, 1)))
+                    {
+                        engine.move(Direction.Up);
+                    }
                 }
             }
             else if (Input.GetKeyDown(KeyCode.Space))
@@ -71,8 +83,16 @@ public class Interface : MonoBehaviour {
             }
         }
     }
-    private bool isEmpty()
+    private bool isEmpty(Vector2 dir)
     {
-        return false;
+        int x = (int)Mathf.Ceil(player.transform.position.x)+(int)dir.x;
+        int y = (int)Mathf.Ceil(player.transform.position.y)+(int)dir.y;
+        foreach(Unit unit  in Database.database.units[x, y])
+        {
+            if (unit.obj.tag == "Container" || unit.obj.tag == "Block" || unit.obj.tag == "wall")
+                return false;
+        }
+       
+        return true;
     }
 }
