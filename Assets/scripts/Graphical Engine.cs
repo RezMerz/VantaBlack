7a7ae66233df10f101bd777bb4f76ff720fd73d0
Reza Mirzaee <rmirzaee95@gmail.com>
2016-10-24 16:24:54 +0330

    gravity added

--- Assets/scripts/Graphical Engine.cs
+++ Assets/scripts/Graphical Engine.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GraphicalEngine{
+public class GraphicalEngine {
 
     Database database;
-
+    private UI ui;
+    private bool fall = false;
+    private int fall_pos;
+    private float gravity = 9.8f;
+    private float velocity = 5;
+    private float fall_distance;
     public GraphicalEngine()
     {
         database = Database.database;
+        ui = GameObject.Find("Canvas").GetComponent<UI>();
     }
 
 	// Use this for initialization
@@ -59,6 +65,7 @@ public class GraphicalEngine{
 
     public void _lean_right()
     {
+        
         database.player.transform.Rotate(new Vector3(0,0,-10));
     }
 
@@ -75,5 +82,46 @@ public class GraphicalEngine{
     public void _lean_left_undo()
     {
         database.player.transform.Rotate(new Vector3(0, 0, -10));
+    }
+
+    public void _right_light()
+    {
+        Wall.print("right light");
+        ui._right_light();
+    }
+
+    public void _gravity()
+    {
+        if (fall)
+        {
+            Vector2 cal = Time.deltaTime * _direction_to_vector(database.gravity_direction) * velocity ;
+            fall_distance += Time.deltaTime * velocity;
+            velocity += Time.deltaTime * gravity;
+            database.player.transform.position = Toolkit.VectorSum(database.player.transform.position, cal);
+            if (fall_distance > fall_pos)
+            {
+                fall = false;
+            }
+        }
+    }
+
+    private Vector2 _direction_to_vector(Direction dir)
+    {
+        if (dir == Direction.Down)
+            return new Vector2(0, -1);
+        else if (dir == Direction.Left)
+            return new Vector2(-1, 0);
+        else if (dir == Direction.Right)
+            return new Vector2(1, 0);
+        else if (dir == Direction.Down)
+            return new Vector2(0, 1);
+        return new Vector2(0, 0);
+    }
+
+    public void _fall_activate(int pos)
+    {
+        fall_distance = 0;
+        fall = true;
+        fall_pos = pos;
     }
 }
