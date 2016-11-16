3d3c2854635706c1746223c2a68f0e892d9ab2e5
Reza Mirzaee <rmirzaee95@gmail.com>
2016-11-16 19:00:15 +0330

    Map COntroller modified

--- Assets/scripts/UI/MapController.cs
+++ Assets/scripts/UI/MapController.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class MapController : MonoBehaviour {
     private bool is_showing;
     private GameObject panel;
+    private GameObject container;
     private GameObject[] rooms;
+
     void Awake()
     {
         panel = gameObject.transform.GetChild(0).gameObject;
+        container = panel.transform.GetChild(0).gameObject;
         panel.SetActive(false);
         is_showing = false;
-        rooms = new GameObject[panel.transform.childCount];
-        print(panel.transform.childCount);
+        rooms = new GameObject[container.transform.childCount];
         int i = 0;
-        foreach(Transform child in panel.transform)
+        foreach(Transform child in container.transform)
         {
             rooms[i] = child.gameObject;
             i++;
@@ -43,14 +46,27 @@ public class MapController : MonoBehaviour {
         int i = 0;
         foreach(GameObject room in rooms)
         {
-            print(i);
             if (check_rooms[i].isVisible)
                 room.SetActive(true);
             else
                 room.SetActive(false);
-            if (Database.database.player.GetComponent<Player>().current_scene == room.name)
+            if (Database.database.player.GetComponent<Player>().current_room == room.name)
                 room.GetComponent<SpriteRenderer>().color = Color.red;
             i++;
+        }
+    }
+
+    public void _update_current_room()
+    {
+
+        Debug.Log(Database.database.player.GetComponent<Player>().current_room);
+        foreach(GameObject room in rooms)
+        {
+            if (room.name == Database.database.player.GetComponent<Player>().current_room)
+            {
+                room.GetComponent<SpriteRenderer>().color = Color.red;
+                
+            }
         }
     }
 }
