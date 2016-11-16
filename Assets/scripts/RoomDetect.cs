ca583dfb966120b3db838df23cc42e1d31446cb8
Reza Mirzaee <rmirzaee95@gmail.com>
2016-11-16 18:30:41 +0330

    Tools created

---
+++ Assets/scripts/RoomDetect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomDetect : MonoBehaviour {
+    public string room;
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Database.database.player.GetComponent<Player>().current_room = room;
+            GameObject.Find("Map").GetComponent<MapController>()._update_current_room();
+            Update_Shadows();
+        }
+    }
+    private void Update_Shadows()
+    {
+        GameObject shadows = GameObject.Find("shadows");
+        foreach(Transform child in shadows.transform)
+        {
+            if (child.gameObject.name != room)
+                child.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            else
+                child.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
