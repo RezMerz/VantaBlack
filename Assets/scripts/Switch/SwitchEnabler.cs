cce55b9e44bb0ea90a16887a925f5a48d971cee1
Aref <valipour.aref@gmail.com>
2016-11-16 19:20:45 +0330

    disabled switch added

---
+++ Assets/scripts/Switch/SwitchEnabler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchEnabler : Switch {
+
+    public Switch otherswitch;
+
+	// Use this for initialization
+	void Start () {
+        unitType = UnitType.Switch;
+        obj = this.gameObject;
+        position = gameObject.transform.position;
+        codeNumber = Code;
+        Code++;
+        movable = false;
+        layer = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override bool Run()
+    {
+        otherswitch.disabled = false;
+        return true;
+    }
+}
